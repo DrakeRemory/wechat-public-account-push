1a215d6aedeb830d9a11be72773eaa16602fb9de
DrakeRemory <78591524+DrakeRemory@users.noreply.github.com>
2023-05-04 16:26:54 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,36 +2,18 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '阿月呀🌙',
     desc: `
       **{{date.DATA}}**
-      
-      下个休息日：{{holidaytts.DATA}}
-      
-      ---
-      
       城市：{{city.DATA}}
-      
       天气：{{weather.DATA}}
-      
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
+
       
       {{comprehensive_horoscope.DATA}}
       
       ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
+
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
   },
